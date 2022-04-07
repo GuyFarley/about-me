3c48d2256b19d9b4145d2d38098605059b80dcf3
Guy Farley <guy.e.farley@gmail.com>
2022-04-06 19:58:06 -0700

    Add questions to guessing game, modify CSS, update README.md

--- README.md
+++ README.md
@@ -14,3 +14,22 @@
 
 - This lab went pretty well, although I did get bogged down in the CSS a bit - just trying to make it look a little cleaner. I'd like to have some key CSS tricks in my back pocket for building simple web sites, since there are so many declarations that I'm still not familiar with. I'm really enjoying the JavaScript portion of these projects so far, and I'm looking forward to diving deeper!
 - This assignment took me about 3 hours to complete, which is about what I expected. It's pretty straightforward, but there's some room for creativity in there.
+
+## Lab 02 - 4.06.22
+
+- For this lab, I added some additional questions and responses that required various loops and if...else statements
+
+### Author: Guy Farley
+
+### Links & Resources
+
+Credit to:
+
+- **Audrey Patterson** for help with my nested for loop on question 7
+- **Katharine Swilley** for providing the solution for conditional statements on question 6
+- **Vinny Shipley** for providing the solution for jumping out of the outer loop on question 7 (`i = 6;`)
+
+### Reflections & Comments
+
+- I struggled a bit with getting my code to execute on the loops. I would like to better understand why the `if...else` statement would not seem to work on my nested loop on question 7
+- I feel pretty good about how I planned out my solutions before writing any code. I was able to put the general structure together pretty easily, but the devil is in the details!
--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+let score = 0;
+
 let visitor = prompt('Hello, what is your name?');
 alert(`Welcome ${visitor}, let's play a guessing game. Please answer Yes or No to the following questions.`);
 
@@ -8,6 +10,7 @@ let questionOne = prompt('Is breakfast my favorite meal of the day?').toLowerCas
 
 if (questionOne === 'yes' || questionOne === 'y') {
   // console.log(`User\'s response to Q1 was '${questionOne}'. Response to user: 'You are correct! I love me some breakfast.'`);
+  score = score + 1;
   alert('You are correct! I love me some breakfast.');
 } else if (questionOne === 'no' || questionOne === 'n') {
   // console.log(`User\'s response to Q1 was '${questionOne}'. Response to user: 'Sorry, that\'s incorrect. Breakfast is the best.'`);
@@ -25,6 +28,7 @@ if (questionTwo === 'yes' || questionTwo === 'y') {
   alert('Sorry, that\'s incorrect. I was born in CA.');
 } else if (questionTwo === 'no' || questionTwo === 'n') {
   // console.log(`User\'s response to Q2 was '${questionTwo}'. Response to user: 'Correct! I was not born in France, I was born in California'`);
+  score = score + 1;
   alert('Correct! I was NOT born in France, I was born in California');
 } else {
   // console.log(`User\'s response to Q2 was '${questionTwo}'. Response to user: 'Please answer with a valid response (Yes or No)!'`);
@@ -39,6 +43,7 @@ if (questionThree === 'yes' || questionThree === 'y') {
   alert('Nope, never been. I\'m not a fan of heights.');
 } else if (questionThree === 'no' || questionThree === 'n') {
   // console.log(`User\'s response to Q3 was '${questionThree}'. Response to user: 'That\'s right. I\'m not jumping out of a plane unless it\'s on fire.'`);
+  score = score + 1;
   alert('That\'s right. I\'m not jumping out of a plane unless it\'s on fire.');
 } else {
   // console.log(`User\'s response to Q3 was '${questionThree}'. Response to user: 'Please answer with a valid response (Yes or No)!'`);
@@ -53,6 +58,7 @@ if (questionFour === 'yes' || questionFour === 'y') {
   alert('Sorry, that\'s incorrect.');
 } else if (questionFour === 'no' || questionFour === 'n') {
   // console.log(`User\'s response to Q4 was '${questionFour}'. Response to user: 'That\'s correct. I\'m more of an alternative rock guy.'`);
+  score = score + 1;
   alert('That\'s correct. I\'m more of an alternative rock guy.');
 } else {
   // console.log(`User\'s response to Q4 was '${questionFour}'. Response to user: 'Please answer with a valid response (Yes or No)!'`);
@@ -64,6 +70,7 @@ let questionFive = prompt('Was "No Country For Old Men" my favorite movie of 200
 
 if (questionFive === 'yes' || questionFive === 'y') {
   // console.log(`User\'s response to Q5 was '${questionFive}'. Response to user: 'That's correct, ${visitor}! Although 2007 was a strong year for movies, that one was my favorite.'`);
+  score = score + 1;
   alert(`That's correct, ${visitor}! Although 2007 was a strong year for movies, that one was my favorite.`);
 } else if (questionFive === 'no' || questionFive === 'n') {
   // console.log(`User\'s response to Q5 was '${questionFive}'. Response to user: 'Sorry ${visitor}, that\'s incorrect. You\'re probably thinking it was "There Will Be Blood" but that was actually my 2nd favorite.'`);
@@ -72,3 +79,43 @@ if (questionFive === 'yes' || questionFive === 'y') {
   // console.log(`User\'s response to Q5 was '${questionFive}'. Response to user: 'Please answer with a valid response (Yes or No)!'`);
   alert(`Sorry ${visitor}, please answer with a valid response (Yes or No)!`);
 }
+
+// prompt and alert #6
+let myNum = 9;
+for (let i = 0; i < 4; i++) {
+
+  let questionSix = parseInt(prompt('Please guess a number between 1 and 10'));
+  if (questionSix === myNum) {
+    score = score + 1;
+    alert('That\'s correct!');
+    break;
+  } else if ((questionSix < myNum) && (i <= 2)) {
+    alert('Your number is too low! Please try again.');
+  } else if ((questionSix > myNum) && (i <= 2)) {
+    alert('Your number is too high! Please try again.');
+  } else if ((questionSix !== myNum) && (i === 3)) {
+    alert('Sorry, you are out of attempts! The correct answer was 9.');
+  }
+}
+
+// prompt and alert #7
+let myFoods = ['pancakes', 'steak', 'cereal'];
+// console.log(myFoods);
+
+for (let i = 0; i < 6; i++) {
+  let questionSeven = prompt(`What is one of my favorite foods? You get 6 tries...`).toLowerCase();
+  // console.log(questionSeven);
+  for (let f = 0; f < myFoods.length; f++) {
+    if (questionSeven === myFoods[f]) {
+      score = score + 1;
+      alert(`That's correct!`);
+      i = 6;
+    }
+  }
+}
+
+alert(`I would have accepted any of the following answers: ${myFoods[0]}, ${myFoods[1]}, or ${myFoods[2]}.`);
+// console.log(`I would have accepted any of the following answers: ${myFoods[0]}, ${myFoods[1]}, or ${myFoods[2]}.`);
+
+alert(`You guessed ${score} out of 7 questions correctly!`);
+// console.log(`You guessed ${score} out of 7 questions correctly!`);
