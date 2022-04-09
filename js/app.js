34c93d6b4d8a1f78d8f37ff07f000bd228451f72
Guy Farley <guy.e.farley@gmail.com>
2022-04-08 17:14:05 -0700

    Remove all comments and console.log's

--- js/app.js
+++ js/app.js
@@ -3,95 +3,78 @@
 let score = 0;
 
 let visitor = prompt('Hello, what is your name?');
-alert(`Welcome ${visitor}, let's play a guessing game. Please answer Yes or No to the following questions.`);
+alert(`Welcome ${visitor}, let's play a guessing game. Please answer "Yes" or "No" to the following questions.`);
 
-// prompt and alert #1
 function q1() {
   let questionOne = prompt('Is breakfast my favorite meal of the day?').toLowerCase();
 
   if (questionOne === 'yes' || questionOne === 'y') {
-    // console.log(`User\'s response to Q1 was '${questionOne}'. Response to user: 'You are correct! I love me some breakfast.'`);
     score = score + 1;
     alert('You are correct! I love me some breakfast.');
   } else if (questionOne === 'no' || questionOne === 'n') {
-    // console.log(`User\'s response to Q1 was '${questionOne}'. Response to user: 'Sorry, that\'s incorrect. Breakfast is the best.'`);
     alert('Sorry, that\'s incorrect. Breakfast is the best.');
   } else {
-    // console.log(`User\'s response to Q1 was '${questionOne}'. Response to user: 'Please answer with a valid response (Yes or No)!'`);
     alert('Please answer with a valid response (Yes or No)!');
   }
 }
 q1();
-// prompt and alert #2
+
 function q2() {
   let questionTwo = prompt('Was I born in France?').toLowerCase();
 
   if (questionTwo === 'yes' || questionTwo === 'y') {
-    // console.log(`User\'s response to Q2 was '${questionTwo}'. Response to user: 'Sorry, that\'s incorrect. I was born in CA.'`);
     alert('Sorry, that\'s incorrect. I was born in CA.');
   } else if (questionTwo === 'no' || questionTwo === 'n') {
-    // console.log(`User\'s response to Q2 was '${questionTwo}'. Response to user: 'Correct! I was not born in France, I was born in California'`);
     score = score + 1;
     alert('Correct! I was NOT born in France, I was born in California');
   } else {
-    // console.log(`User\'s response to Q2 was '${questionTwo}'. Response to user: 'Please answer with a valid response (Yes or No)!'`);
     alert('Please answer with a valid response (Yes or No)!');
   }
 }
 q2();
-// prompt and alert #3
+
 function q3() {
   let questionThree = prompt('Have I ever been skydiving?').toLowerCase();
 
   if (questionThree === 'yes' || questionThree === 'y') {
-    // console.log(`User\'s response to Q3 was '${questionThree}'. Response to user: 'Nope, never been. I\'m not a fan of heights.'`);
     alert('Nope, never been. I\'m not a fan of heights.');
   } else if (questionThree === 'no' || questionThree === 'n') {
-    // console.log(`User\'s response to Q3 was '${questionThree}'. Response to user: 'That\'s right. I\'m not jumping out of a plane unless it\'s on fire.'`);
     score = score + 1;
     alert('That\'s right. I\'m not jumping out of a plane unless it\'s on fire.');
   } else {
-    // console.log(`User\'s response to Q3 was '${questionThree}'. Response to user: 'Please answer with a valid response (Yes or No)!'`);
     alert('Please answer with a valid response (Yes or No)!');
   }
 }
 q3();
 
-// prompt and alert #4
 function q4() {
   let questionFour = prompt('Is my favorite music genre "country"?').toLowerCase();
 
   if (questionFour === 'yes' || questionFour === 'y') {
-    // console.log(`User\'s response to Q4 was '${questionFour}'. Response to user: 'Sorry, that\'s incorrect.'`);
     alert('Sorry, that\'s incorrect.');
   } else if (questionFour === 'no' || questionFour === 'n') {
-    // console.log(`User\'s response to Q4 was '${questionFour}'. Response to user: 'That\'s correct. I\'m more of an alternative rock guy.'`);
     score = score + 1;
     alert('That\'s correct. I\'m more of an alternative rock guy.');
   } else {
-    // console.log(`User\'s response to Q4 was '${questionFour}'. Response to user: 'Please answer with a valid response (Yes or No)!'`);
     alert('Please answer with a valid response (Yes or No)!');
   }
 }
 q4();
-// prompt and alert #5
+
 function q5() {
   let questionFive = prompt('Was "No Country For Old Men" my favorite movie of 2007?').toLowerCase();
 
   if (questionFive === 'yes' || questionFive === 'y') {
-    // console.log(`User\'s response to Q5 was '${questionFive}'. Response to user: 'That's correct, ${visitor}! Although 2007 was a strong year for movies, that one was my favorite.'`);
     score = score + 1;
     alert(`That's correct, ${visitor}! Although 2007 was a strong year for movies, that one was my favorite.`);
   } else if (questionFive === 'no' || questionFive === 'n') {
-    // console.log(`User\'s response to Q5 was '${questionFive}'. Response to user: 'Sorry ${visitor}, that\'s incorrect. You\'re probably thinking it was "There Will Be Blood" but that was actually my 2nd favorite.'`);
     alert(`Sorry ${visitor}, that's incorrect. You're probably thinking it was "There Will Be Blood" but that was actually my 2nd favorite.`);
   } else {
-    // console.log(`User\'s response to Q5 was '${questionFive}'. Response to user: 'Please answer with a valid response (Yes or No)!'`);
     alert(`Sorry ${visitor}, please answer with a valid response (Yes or No)!`);
   }
 }
 q5();
-// prompt and alert #6
+
 function q6() {
   let myNum = 9;
   for (let i = 0; i < 4; i++) {
@@ -111,16 +94,12 @@ function q6() {
   }
 }
 q6();
-// prompt and alert #7
 
 function q7() {
   let myFoods = ['pancakes', 'steak', 'cereal'];
 
-  // console.log(myFoods);
-
   for (let i = 0; i < 6; i++) {
     let questionSeven = prompt(`What is one of my favorite foods? You get 6 tries...`).toLowerCase();
-    // console.log(questionSeven);
     for (let f = 0; f < myFoods.length; f++) {
       if (questionSeven === myFoods[f]) {
         score = score + 1;
@@ -130,9 +109,7 @@ function q7() {
     }
   }
   alert(`I would have accepted any of the following answers: ${myFoods[0]}, ${myFoods[1]}, or ${myFoods[2]}.`);
-  // console.log(`I would have accepted any of the following answers: ${myFoods[0]}, ${myFoods[1]}, or ${myFoods[2]}.`);
 }
 q7();
 
 alert(`You guessed ${score} out of 7 questions correctly!`);
-// console.log(`You guessed ${score} out of 7 questions correctly!`);
