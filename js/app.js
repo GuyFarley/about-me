2beb69e43c9159d113d3265c6dd19cc5dcb79b86
Guy Farley <guy.e.farley@gmail.com>
2022-04-09 15:13:45 -0700

    Consolidate questions 1 - 5 into one function that calls arrays, and modified guess-a-number question to choose a random number

--- js/app.js
+++ js/app.js
@@ -5,78 +5,49 @@ let score = 0;
 let visitor = prompt('Hello, what is your name?');
 alert(`Welcome ${visitor}, let's play a guessing game. Please answer "Yes" or "No" to the following questions.`);
 
-function q1() {
-  let questionOne = prompt('Is breakfast my favorite meal of the day?').toLowerCase();
+let questArray = [
+  `Is breakfast my favorite meal of the day?`, //0
+  `Was I born in France?`, //1
+  `Have I ever been skydiving?`, //2
+  `Is my favorite music genre "country"?`, //3
+  `Was "No Country For Old Men" my favorite movie of 2007?`] //4
 
-  if (questionOne === 'yes' || questionOne === 'y') {
-    score = score + 1;
-    alert('You are correct! I love me some breakfast.');
-  } else if (questionOne === 'no' || questionOne === 'n') {
-    alert('Sorry, that\'s incorrect. Breakfast is the best.');
-  } else {
-    alert('Please answer with a valid response (Yes or No)!');
+let respArray = [
+  null, //0
+  `You are correct! I love me some breakfast.`, //1
+  `Sorry, that's incorrect. Breakfast is the best.`, //2
+  `Sorry, that's incorrect. I was born in CA.`, //3
+  `Correct! I was NOT born in France, I was born in California`, //4
+  `Nope, never been. I'm not a fan of heights.`, //5
+  `That's right. I'm not jumping out of a plane unless it's on fire.`, //6
+  `Sorry, that's incorrect.`, //7
+  `That's correct. I'm more of an alternative rock guy.`, //8
+  `That's correct, ${visitor}! Although 2007 was a strong year for movies, that one was my favorite.`, //9
+  `Sorry ${visitor}, that's incorrect. You're probably thinking it was "There Will Be Blood" but that was actually my 2nd favorite.`, //10
+  `Sorry ${visitor}, please answer with a valid response (Yes or No)!`] //11
+
+let n = 2;
+
+function guessGame() {
+  for (let i = 0; i < 5; i++) {
+    let question = prompt(questArray[i]).toLowerCase();
+
+    if (question === 'yes' || question === 'y') {
+      score = score + 1;
+      alert(respArray[n - 1]);
+    } else if (question === 'no' || question === 'n') {
+      alert(respArray[n]);
+    } else {
+      alert(respArray[11]);
+    }
+    n = n + 2;
   }
 }
-q1();
-
-function q2() {
-  let questionTwo = prompt('Was I born in France?').toLowerCase();
-
-  if (questionTwo === 'yes' || questionTwo === 'y') {
-    alert('Sorry, that\'s incorrect. I was born in CA.');
-  } else if (questionTwo === 'no' || questionTwo === 'n') {
-    score = score + 1;
-    alert('Correct! I was NOT born in France, I was born in California');
-  } else {
-    alert('Please answer with a valid response (Yes or No)!');
-  }
-}
-q2();
-
-function q3() {
-  let questionThree = prompt('Have I ever been skydiving?').toLowerCase();
-
-  if (questionThree === 'yes' || questionThree === 'y') {
-    alert('Nope, never been. I\'m not a fan of heights.');
-  } else if (questionThree === 'no' || questionThree === 'n') {
-    score = score + 1;
-    alert('That\'s right. I\'m not jumping out of a plane unless it\'s on fire.');
-  } else {
-    alert('Please answer with a valid response (Yes or No)!');
-  }
-}
-q3();
-
-function q4() {
-  let questionFour = prompt('Is my favorite music genre "country"?').toLowerCase();
-
-  if (questionFour === 'yes' || questionFour === 'y') {
-    alert('Sorry, that\'s incorrect.');
-  } else if (questionFour === 'no' || questionFour === 'n') {
-    score = score + 1;
-    alert('That\'s correct. I\'m more of an alternative rock guy.');
-  } else {
-    alert('Please answer with a valid response (Yes or No)!');
-  }
-}
-q4();
-
-function q5() {
-  let questionFive = prompt('Was "No Country For Old Men" my favorite movie of 2007?').toLowerCase();
-
-  if (questionFive === 'yes' || questionFive === 'y') {
-    score = score + 1;
-    alert(`That's correct, ${visitor}! Although 2007 was a strong year for movies, that one was my favorite.`);
-  } else if (questionFive === 'no' || questionFive === 'n') {
-    alert(`Sorry ${visitor}, that's incorrect. You're probably thinking it was "There Will Be Blood" but that was actually my 2nd favorite.`);
-  } else {
-    alert(`Sorry ${visitor}, please answer with a valid response (Yes or No)!`);
-  }
-}
-q5();
+guessGame(questArray, respArray);
 
 function q6() {
-  let myNum = 9;
+  // assigns myNum as a random number between 1 and 10
+  let myNum = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
   for (let i = 0; i < 4; i++) {
 
     let questionSix = parseInt(prompt('Please guess a number between 1 and 10'));
@@ -96,10 +67,10 @@ function q6() {
 q6();
 
 function q7() {
-  let myFoods = ['pancakes', 'steak', 'cereal'];
+  let myFoods = ['pancakes', 'steak', 'cereal', 'bbq', 'waffles', 'pad thai', 'sesame chicken'];
 
   for (let i = 0; i < 6; i++) {
-    let questionSeven = prompt(`What is one of my favorite foods? You get 6 tries...`).toLowerCase();
+    let questionSeven = prompt(`What is one of my favorite foods ? You get 6 tries...`).toLowerCase();
     for (let f = 0; f < myFoods.length; f++) {
       if (questionSeven === myFoods[f]) {
         score = score + 1;
@@ -108,8 +79,9 @@ function q7() {
       }
     }
   }
-  alert(`I would have accepted any of the following answers: ${myFoods[0]}, ${myFoods[1]}, or ${myFoods[2]}.`);
+  alert(`I would have accepted any of the following answers: ${myFoods[0]}, ${myFoods[1]}, ${myFoods[2]}, ${myFoods[3]}, ${myFoods[4]}, ${myFoods[5]}, or ${myFoods[6]}.`);
 }
 q7();
 
 alert(`You guessed ${score} out of 7 questions correctly!`);
+
